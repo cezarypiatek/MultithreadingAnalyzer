036356011d8230bc130d19b0e7e07b347b3d77b0
cezarypiatek <cezarypiatek@live.com>
2019-09-03 22:40:34 +0200

    Replace test framework to RoslynTestKit

--- src/MultithreadingAnalyzer.Test/LockObjectSelection/LockObjectSelectionAnalyzerTests.cs
+++ src/MultithreadingAnalyzer.Test/LockObjectSelection/LockObjectSelectionAnalyzerTests.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using NUnit.Framework;
-using RoslynNUnitLight;
+using RoslynTestKit;
 
 namespace SmartAnalyzers.MultithreadingAnalyzer.Test.LockObjectSelection
 {
@@ -13,261 +10,232 @@ namespace SmartAnalyzers.MultithreadingAnalyzer.Test.LockObjectSelection
         [Test]
         public void do_not_lock_on_non_readonly_field()
         {
-            HasDiagnostic(TestCases._001_LockOnNonReadonlyField, 11, nameof(LockObjectSelectionAnalyzer.MT1003));
+            HasDiagnosticAtLine(TestCases._001_LockOnNonReadonlyField, nameof(LockObjectSelectionAnalyzer.MT1003), 11);
         }
 
         [Test]
         public void do_not_monitor_enter_on_non_readonly_field()
         {
-            HasDiagnostic(TestCases._001_LockOnNonReadonlyField, 18, nameof(LockObjectSelectionAnalyzer.MT1003));
+            HasDiagnosticAtLine(TestCases._001_LockOnNonReadonlyField, nameof(LockObjectSelectionAnalyzer.MT1003), 18);
         }
 
         [Test]
         public void do_not_monitor_try_enter_on_non_readonly_field()
         {
-            HasDiagnostic(TestCases._001_LockOnNonReadonlyField, 25, nameof(LockObjectSelectionAnalyzer.MT1003));
+            HasDiagnosticAtLine(TestCases._001_LockOnNonReadonlyField, nameof(LockObjectSelectionAnalyzer.MT1003), 25);
         }
 
         [Test]
         public void do_not_lock_on_non_readonly_property()
         {
-            HasDiagnostic(TestCases._002_LockOnNonReadonlyProperty, 11, nameof(LockObjectSelectionAnalyzer.MT1003));
+            HasDiagnosticAtLine(TestCases._002_LockOnNonReadonlyProperty, nameof(LockObjectSelectionAnalyzer.MT1003), 11);
         }
 
         [Test]
         public void do_not_monitor_enter_on_non_readonly_property()
         {
-            HasDiagnostic(TestCases._002_LockOnNonReadonlyProperty, 18, nameof(LockObjectSelectionAnalyzer.MT1003));
+            HasDiagnosticAtLine(TestCases._002_LockOnNonReadonlyProperty, nameof(LockObjectSelectionAnalyzer.MT1003), 18);
         }
 
         [Test]
         public void do_not_monitor_try_enter_on_non_readonly_property()
         {
-            HasDiagnostic(TestCases._002_LockOnNonReadonlyProperty, 25, nameof(LockObjectSelectionAnalyzer.MT1003));
+            HasDiagnosticAtLine(TestCases._002_LockOnNonReadonlyProperty, nameof(LockObjectSelectionAnalyzer.MT1003), 25);
         }
 
         [Test]
         public void do_not_lock_on_public_field()
         {
-            HasDiagnostic(TestCases._003_LockOnPublicField, 11, nameof(LockObjectSelectionAnalyzer.MT1000));
+            HasDiagnosticAtLine(TestCases._003_LockOnPublicField, nameof(LockObjectSelectionAnalyzer.MT1000), 11);
         }
 
         [Test]
         public void do_not_monitor_enter_on_public_field()
         {
-            HasDiagnostic(TestCases._003_LockOnPublicField, 18, nameof(LockObjectSelectionAnalyzer.MT1000));
+            HasDiagnosticAtLine(TestCases._003_LockOnPublicField, nameof(LockObjectSelectionAnalyzer.MT1000), 18);
         }
 
         [Test]
         public void do_not_monitor_try_enter_on_public_field()
         {
-            HasDiagnostic(TestCases._003_LockOnPublicField, 25, nameof(LockObjectSelectionAnalyzer.MT1000));
+            HasDiagnosticAtLine(TestCases._003_LockOnPublicField, nameof(LockObjectSelectionAnalyzer.MT1000), 25);
         }
 
         [Test]
         public void do_not_lock_on_public_property()
         {
-            HasDiagnostic(TestCases._004_LockOnPublicProperty, 11, nameof(LockObjectSelectionAnalyzer.MT1000));
+            HasDiagnosticAtLine(TestCases._004_LockOnPublicProperty, nameof(LockObjectSelectionAnalyzer.MT1000), 11);
         }
 
         [Test]
         public void do_not_monitor_enter_on_public_property()
         {
-            HasDiagnostic(TestCases._004_LockOnPublicProperty, 18, nameof(LockObjectSelectionAnalyzer.MT1000));
+            HasDiagnosticAtLine(TestCases._004_LockOnPublicProperty, nameof(LockObjectSelectionAnalyzer.MT1000), 18);
         }
 
         [Test]
         public void do_not_monitor_try_enter_on_public_property()
         {
-            HasDiagnostic(TestCases._004_LockOnPublicProperty, 25, nameof(LockObjectSelectionAnalyzer.MT1000));
+            HasDiagnosticAtLine(TestCases._004_LockOnPublicProperty, nameof(LockObjectSelectionAnalyzer.MT1000), 25);
         }
 
 
         [Test]
         public void do_not_monitor_enter_on_this_reference()
         {
-            HasDiagnostic(TestCases._005_LockOnThisInstance, 10, nameof(LockObjectSelectionAnalyzer.MT1001));
+            HasDiagnosticAtLine(TestCases._005_LockOnThisInstance, nameof(LockObjectSelectionAnalyzer.MT1001), 10);
         }
 
         [Test]
         public void do_not_monitor_try_enter_on_this_reference()
         {
-            HasDiagnostic(TestCases._005_LockOnThisInstance, 17, nameof(LockObjectSelectionAnalyzer.MT1001));
+            HasDiagnosticAtLine(TestCases._005_LockOnThisInstance, nameof(LockObjectSelectionAnalyzer.MT1001), 17);
         }
 
         [Test]
         public void do_not_monitor_enter_on_value_type()
         {
-            HasDiagnostic(TestCases._006_LockOnValueType, 16, nameof(LockObjectSelectionAnalyzer.MT1004));
+            HasDiagnosticAtLine(TestCases._006_LockOnValueType, nameof(LockObjectSelectionAnalyzer.MT1004), 16);
         }
 
         [Test]
         public void do_not_monitor_try_enter_on_value_type()
         {
-            HasDiagnostic(TestCases._006_LockOnValueType, 23, nameof(LockObjectSelectionAnalyzer.MT1004));
+            HasDiagnosticAtLine(TestCases._006_LockOnValueType, nameof(LockObjectSelectionAnalyzer.MT1004), 23);
         }
 
         [Test]
         public void do_not_lock_on_typeof()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 14, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 14);
         }
 
         [Test]
         public void do_not_lock_on_type_object()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 22, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 22);
         }
 
         [Test]
         public void do_not_monitor_on_type_object()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 30, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 30);
         }
 
         [Test]
         public void do_not_monitor_try_on_type_object()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 37, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 37);
         }
 
         [Test]
         public void do_not_lock_on_value_array()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 48, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 48);
         }
 
         [Test]
         public void do_not_monitor_on_value_array()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 56, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 56);
         }
 
         [Test]
         public void do_not_monitor_try_on_value_array()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 63, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 63);
         }
 
         [Test]
         public void do_not_lock_on_thread()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 74, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 74);
         }
 
         [Test]
         public void do_not_monitor_on_thread()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 82, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 82);
         }
 
         [Test]
         public void do_not_monitor_try_on_thread()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 89, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 89);
         }
 
         [Test]
         public void do_not_lock_on_string()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 100, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 100);
         }
 
         [Test]
         public void do_not_monitor_on_string()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 108, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 108);
         }
 
         [Test]
         public void do_not_monitor_try_on_string()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 115, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 115);
         }
 
         [Test]
         public void do_not_lock_on_parameter_info()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 128, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 128);
         }
 
         [Test]
         public void do_not_monitor_on_parameter_info()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 136, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 136);
         }
 
         [Test]
         public void do_not_monitor_try_on_parameter_info()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 143, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 143);
         }
 
         [Test]
         public void do_not_lock_on_marshal_by_ref()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 158, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 158);
         }
 
         [Test]
         public void do_not_monitor_on_marshal_by_ref()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 166, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 166);
         }
 
         [Test]
         public void do_not_monitor_try_on_marshal_by_ref()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 184, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 184);
         }
 
         [Test]
         public void do_not_lock_on_exception()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 158, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 158);
         }
 
         [Test]
         public void do_not_monitor_on_exception()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 192, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 192);
         }
 
         [Test]
         public void do_not_monitor_try_on_exception()
         {
-            HasDiagnostic(TestCases._007_LockOnObjectWithWeakIdentity, 199, nameof(LockObjectSelectionAnalyzer.MT1002));
+            HasDiagnosticAtLine(TestCases._007_LockOnObjectWithWeakIdentity, nameof(LockObjectSelectionAnalyzer.MT1002), 199);
         }
-
-        protected void HasDiagnostic(string code, int line, string diagnosticId)
-        {
-            var zeroBaseLine = line - 1;
-            var document = TestHelpers.GetDocument(code, this.LanguageName, (ImmutableList<MetadataReference>) null);
-            var diagnostics = this.GetDiagnostics(document);
-            var diagnostic = diagnostics.FirstOrDefault(x => 
-                x.Id == diagnosticId && 
-                x.Location.GetLineSpan().StartLinePosition.Line <= zeroBaseLine &&
-                x.Location.GetLineSpan().EndLinePosition.Line >= zeroBaseLine &&
-                x.Location.IsInSource);
-            Assert.IsNotNull(diagnostic, $"There is no diagnostic {diagnosticId} at line {line}");
-        }
-
-        private ImmutableArray<Diagnostic> GetDiagnostics(Document document)
-        {
-            ImmutableArray<DiagnosticAnalyzer> immutableArray = ImmutableArray.Create<DiagnosticAnalyzer>(this.CreateAnalyzer());
-            Compilation result1 = document.Project.GetCompilationAsync(CancellationToken.None).Result;
-            CompilationWithAnalyzers compilationWithAnalyzers = DiagnosticAnalyzerExtensions.WithAnalyzers(result1, immutableArray, (AnalyzerOptions)null, CancellationToken.None);
-            result1.GetDiagnostics(CancellationToken.None);
-            SyntaxTree result2 = document.GetSyntaxTreeAsync(CancellationToken.None).Result;
-            ImmutableArray<Diagnostic>.Builder builder = ImmutableArray.CreateBuilder<Diagnostic>();
-            foreach (Diagnostic diagnostic in compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync().Result)
-            {
-                Location location = diagnostic.Location;
-                if (location.IsInSource && location.SourceTree == result2)
-                    builder.Add(diagnostic);
-            }
-            return builder.ToImmutable();
-        }
+    
         protected override string LanguageName => LanguageNames.CSharp;
 
         protected override DiagnosticAnalyzer CreateAnalyzer()
--- src/MultithreadingAnalyzer.Test/MethodLevelSynchronization/MethodLevelSynchronizationAnalyzerTests.cs
+++ src/MultithreadingAnalyzer.Test/MethodLevelSynchronization/MethodLevelSynchronizationAnalyzerTests.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using NUnit.Framework;
-using RoslynNUnitLight;
+using RoslynTestKit;
 using static SmartAnalyzers.MultithreadingAnalyzer.Test.MethodLevelSynchronization.TestCases;
 
 namespace SmartAnalyzers.MultithreadingAnalyzer.Test.MethodLevelSynchronization
@@ -14,44 +11,15 @@ namespace SmartAnalyzers.MultithreadingAnalyzer.Test.MethodLevelSynchronization
         [Test]
         public void do_not_use_method_level_synchronization()
         {
-            HasDiagnostic(_001_MethodLevelSyncrhonization, 9, MethodLevelSynchronizationAnalyzer.DiagnosticId);
+            HasDiagnosticAtLine(_001_MethodLevelSyncrhonization,  MethodLevelSynchronizationAnalyzer.DiagnosticId,9 );
         }
 
         [Test]
         public void do_not_use_method_level_synchronization_multiple_flags()
         {
-            HasDiagnostic(_001_MethodLevelSyncrhonization, 14, MethodLevelSynchronizationAnalyzer.DiagnosticId);
+            HasDiagnosticAtLine(_001_MethodLevelSyncrhonization, MethodLevelSynchronizationAnalyzer.DiagnosticId, 14);
         }
-
-        protected void HasDiagnostic(string code, int line, string diagnosticId)
-        {
-            var zeroBaseLine = line - 1;
-            var document = TestHelpers.GetDocument(code, this.LanguageName, (ImmutableList<MetadataReference>) null);
-            var diagnostics = this.GetDiagnostics(document);
-            var diagnostic = diagnostics.FirstOrDefault(x => 
-                x.Id == diagnosticId && 
-                x.Location.GetLineSpan().StartLinePosition.Line <= zeroBaseLine &&
-                x.Location.GetLineSpan().EndLinePosition.Line >= zeroBaseLine &&
-                x.Location.IsInSource);
-            Assert.IsNotNull(diagnostic, $"There is no diagnostic {diagnosticId} at line {line}");
-        }
-
-        private ImmutableArray<Diagnostic> GetDiagnostics(Document document)
-        {
-            ImmutableArray<DiagnosticAnalyzer> immutableArray = ImmutableArray.Create<DiagnosticAnalyzer>(this.CreateAnalyzer());
-            Compilation result1 = document.Project.GetCompilationAsync(CancellationToken.None).Result;
-            CompilationWithAnalyzers compilationWithAnalyzers = DiagnosticAnalyzerExtensions.WithAnalyzers(result1, immutableArray, (AnalyzerOptions)null, CancellationToken.None);
-            result1.GetDiagnostics(CancellationToken.None);
-            SyntaxTree result2 = document.GetSyntaxTreeAsync(CancellationToken.None).Result;
-            ImmutableArray<Diagnostic>.Builder builder = ImmutableArray.CreateBuilder<Diagnostic>();
-            foreach (Diagnostic diagnostic in compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync().Result)
-            {
-                Location location = diagnostic.Location;
-                if (location.IsInSource && location.SourceTree == result2)
-                    builder.Add(diagnostic);
-            }
-            return builder.ToImmutable();
-        }
+        
         protected override string LanguageName => LanguageNames.CSharp;
 
         protected override DiagnosticAnalyzer CreateAnalyzer()
